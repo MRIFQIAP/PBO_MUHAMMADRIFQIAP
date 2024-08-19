86a2d9452ad83778a7557f328a3560b94d250a81
MRIFQIAP <mrifqiajipratama@gmail.com>
2024-08-19 08:46:20 +0700

    Add files via upload

---
+++ Latihan1.1.php
@@ -0,0 +1,13 @@
+class mobil
+{
+    //body class
+    var $jumlahroda = 4;
+    var $warna = "Merah";
+    var $bahanbakar = "Pertamax";
+    var $harga = 1200000000
+    var $merk = 'A';
+}
+
+$objekBMW = new mobil ;
+$objekTESLA = new mobil ;
+$objekAUDI = new mobil ;
